9aea27aadd20e6b9e59f7b7ae9cf819e1cb9e250
Adam Tolaini <adam.tolaini@gmail.com>
2018-05-15 12:07:17 +0100

    First Commit all base setup complete

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './styling/css/main.css'
+import Layout from './hoc/Layout/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        Test
+      </Layout>
+    );
+  }
+}
+
+export default App;
